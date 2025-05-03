d2286f0ff9a41ace182c427c467876951e48aa1b
x1n4px <silosabia@gmail.com>
2025-05-03 19:46:13 +0200

    act

--- back2/api/url.js
+++ back2/api/url.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const { createClient } = require('@libsql/client');
+const { v4: uuidv4 } = require('uuid');
 
 
 // Health Check Endpoint
