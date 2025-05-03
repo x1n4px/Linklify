8e9cc085159458368e3a847291ad88c7e55e49c7
x1n4px <silosabia@gmail.com>
2025-05-03 19:07:11 +0200

    act

--- linklify-backend/api/index.ts
+++ linklify-backend/api/index.ts
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 const URLRoute = require('./routes/urlRoute')
-const serverless = require("serverless-http");
 
 
 app.use(cors());
@@ -15,4 +14,10 @@ app.get("/", (req, res) => {
 app.use('/api', URLRoute);
 
 
-module.exports.handler = serverless(app);
+const port = 3005;
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
+module.exports = app;
